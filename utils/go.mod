ab62e9c50d71e53a5ad12c9e6e3a702c4119b60a
Acho Arnold <a.ewin@geophy.com>
2020-01-13 07:28:27 +0100

    Added go mod

---
+++ utils/go.mod
@@ -0,0 +1,3 @@
+module github.com/AchoArnold/cracking-the-coding-interview/util
+
+go 1.13
--- utils/testing_util.go
+++ utils/testing_util.go
@@ -1,4 +1,4 @@
-package utils
+package util
 
 // IfThenElse evaluates a condition, if true returns the first parameter otherwise the second
 func IfThenElse(condition bool, a interface{}, b interface{}) interface{} {
