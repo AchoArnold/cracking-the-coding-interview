c936c78ce2cf7e793c4e6fe2600a70e94e96fc4d
Acho Arnold <a.ewin@geophy.com>
2020-02-11 13:39:57 +0100

    Added the implementation for a multiarray stack

---
+++ chapter3/1_multi_stack_array.go
@@ -0,0 +1,72 @@
+package chapter3
+
+import (
+	"errors"
+	"fmt"
+)
+
+// We assume the 3 stacks of length = 4 each
+type MultiArrayStack struct {
+	stackIndexes [3]int
+	stack [12]int
+}
+
+const index0 = 0
+const index1 = 4
+const index2 = 8
+
+func NewMultiArrayStack() *MultiArrayStack {
+	stack := MultiArrayStack{getDefaultIndexes(), [12]int{}}
+	return &stack
+}
+
+func getDefaultIndexes() [3]int {
+	return [3]int{index0, index1, index2}
+}
+
+func (stack *MultiArrayStack) Pop(stackIndex int) (int, error) {
+	stack.AssertStackIndexIsWithinRange(stackIndex)
+
+	if stack.IsEmpty(stackIndex) {
+		return -1, errors.New(fmt.Sprintf("Cannot pop from stack %d because it is empty", stackIndex))
+	}
+
+	value := stack.stack[stack.stackIndexes[stackIndex] - 1]
+	stack.stackIndexes[stackIndex]--
+	return value, nil
+}
+
+
+func (stack *MultiArrayStack) Push(stackIndex int, data int) error {
+	stack.AssertStackIndexIsWithinRange(stackIndex)
+
+	if stack.stackIndexes[stackIndex] == getDefaultIndexes()[stackIndex] + 4 {
+		return errors.New(fmt.Sprintf("Cannot push into stack %d because it is full", stackIndex))
+	}
+
+	stack.stack[stack.stackIndexes[stackIndex]] = data
+	stack.stackIndexes[stackIndex]++
+
+	return nil
+}
+
+func (stack *MultiArrayStack) Peek(stackIndex int, data int) (int, error) {
+	stack.AssertStackIndexIsWithinRange(stackIndex)
+
+	if stack.IsEmpty(stackIndex) {
+		return -1, errors.New(fmt.Sprintf("Cannot peek into stack %d because it is empty", stackIndex))
+	}
+
+	return stack.stack[stack.stackIndexes[stackIndex]], nil
+}
+
+func (stack *MultiArrayStack) IsEmpty(stackIndex int) bool {
+	stack.AssertStackIndexIsWithinRange(stackIndex)
+	return stack.stackIndexes[stackIndex] == getDefaultIndexes()[stackIndex]
+}
+
+func (stack *MultiArrayStack) AssertStackIndexIsWithinRange(stackIndex int) {
+	if stackIndex < 0 || stackIndex > 2 {
+		panic(fmt.Sprintf("The stack index = %d is out of range", stackIndex))
+	}
+}
---
+++ chapter3/1_multi_stack_array_test.go
@@ -0,0 +1,106 @@
+package chapter3
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+
+func TestMultiArrayStack_Pop(t *testing.T) {
+	stack := MultiArrayStack{
+		[3]int {1,5,9},
+		[12]int{4,0,0,0,3,0,0,0,2,0,0,0},
+	}
+
+	for index, val := range []int{4, 3, 2 } {
+		t.Run("Test the stack returns the element at the top of the stack", func(t *testing.T) {
+			element, _ := stack.Pop(index)
+			assert.Equal(t, val, element)
+		})
+	}
+
+	for index, _:= range []int{4, 3, 2 } {
+		t.Run("Test the stack returns an error when the stack is empty", func(t *testing.T) {
+			element, err := stack.Pop(index)
+			assert.Equal(t, element, -1)
+			assert.Equal(t, err.Error(), fmt.Sprintf("Cannot pop from stack %d because it is empty", index))
+		})
+	}
+
+	for _, val := range []int{3, 4, -1 } {
+		t.Run("Test a panic is encountered if the stack index is out of range", func(t *testing.T) {
+			assert.Panics(t, func() {stack.Pop(val)})
+		})
+	}
+
+}
+
+func TestMultiArrayStack_IsEmpty(t *testing.T) {
+	stack := MultiArrayStack{
+		[3]int {1,5,9},
+		[12]int{4,0,0,0,3,0,0,0,2,0,0,0},
+	}
+	for index, _ := range []int{4, 3, 2 } {
+		t.Run("IsEmpty returns false when the stack is not empty", func(t *testing.T) {
+			assert.Equal(t, false, stack.IsEmpty(index))
+		})
+	}
+
+	stack = *NewMultiArrayStack()
+	for index, _ := range []int{4, 3, 2 } {
+		t.Run("IsEmpty returns true when the stack is empty", func(t *testing.T) {
+			assert.Equal(t, true, stack.IsEmpty(index))
+		})
+	}
+
+
+	for _, val := range []int{3, 4, -1 } {
+		t.Run("IsEmpty a panics when the stack index is out of range", func(t *testing.T) {
+			assert.Panics(t, func() {stack.IsEmpty(val)})
+		})
+	}
+}
+
+func TestMultiArrayStack_Push(t *testing.T) {
+	var stack MultiArrayStack
+
+	for index,val := range []int{4, 3, 2 } {
+		t.Run("Push adds an item to the stack", func(t *testing.T) {
+			err := stack.Push(index, val)
+			assert.Equal(t, nil, err)
+
+			element, err := stack.Pop(index)
+			assert.Equal(t, val, element)
+			assert.Equal(t, nil, err)
+		})
+	}
+
+	stack = MultiArrayStack{
+		[3]int {4,8,12},
+		[12]int{4,0,0,0,3,0,0,0,2,0,0,0},
+	}
+	for index,val := range []int{4, 3, 2 } {
+		t.Run("Push throws an error if the stack is full", func(t *testing.T) {
+			err := stack.Push(index, val)
+			assert.NotEqual(t, nil, err)
+			assert.Equal(t, fmt.Sprintf("Cannot push into stack %d because it is full", index), err.Error())
+		})
+	}
+
+
+	for _, val := range []int{3, 4, -1 } {
+		t.Run("Push panics when the stack index is out of range", func(t *testing.T) {
+			assert.Panics(t, func() {stack.Push(val, val)})
+		})
+	}
+}
+
+func TestMultiArrayStack_AssertStackIndexIsWithinRange(t *testing.T) {
+	var stack MultiArrayStack
+	for _, val := range []int{3, 4, -1 } {
+		t.Run("AssertStackIndexIsWithinRange a panics when the stack index is out of range", func(t *testing.T) {
+			assert.Panics(t, func() {stack.AssertStackIndexIsWithinRange(val)})
+		})
+	}
+}
